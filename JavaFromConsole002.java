a6c33a2e46c6a64dbaf7b0e2891145546555a1e6
sahyadri551 <sahyadri2413082@akgec.ac.in>
2025-06-17 08:54:33 +0000

    JAVA learning started!

---
+++ JavaFromConsole002.java
@@ -0,0 +1,6 @@
+public class JavaFromConsole002{
+	public static void main(String[] args){
+	System.out.println("This file is created in console, and will be executed through console.\t");
+	System.out.println("sysout => System.out.println()");
+	}
+}
