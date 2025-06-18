0556fb79b204c0801f87b2244a24616c7feb7e64
sahyadri551 <sahyadri2413082@akgec.ac.in>
2025-06-18 09:27:57 +0530

    Rename old Java files HelloWorld and JavaFromConsole implementations

--- J001HelloWorld.java
+++ J001HelloWorld.java
@@ -1,4 +1,4 @@
-public class HelloWorld001 {
+public class J001HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World! My first Java program.\n");
     }
--- J002JavaFromConsole.java
+++ J002JavaFromConsole.java
@@ -1,4 +1,4 @@
-public class JavaFromConsole002{
+public class J002JavaFromConsole{
 	public static void main(String[] args){
 	System.out.println("This file is created in console, and will be executed through console.\t");
 	System.out.println("sysout => System.out.println()");
