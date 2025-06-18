92e30664df44940a55725cc1d7e469aa70e1bad1
sahyadri551 <sahyadri2413082@akgec.ac.in>
2025-06-18 12:46:50 +0530

    Add J004NumberSystem class to demonstrate number systems in Java

---
+++ J004NumberSystem.java
@@ -0,0 +1,19 @@
+public class J004NumberSystem {
+    public static void main(String[] args) {
+        // Decimal (base 10)
+        int decimal = 255;
+        System.out.println("Decimal: " + decimal);
+
+        // Binary (base 2)
+        int binary = 0b11111111; // 255 in binary
+        System.out.println("Binary: " + binary);
+
+        // Octal (base 8)
+        int octal = 0377; // 255 in octal
+        System.out.println("Octal: " + octal);
+
+        // Hexadecimal (base 16)
+        int hexadecimal = 0xFF; // 255 in hexadecimal
+        System.out.println("Hexadecimal: " + hexadecimal);
+    }
+}
