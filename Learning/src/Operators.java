1a361e23b12d81f5e2819cb7148818dae1d85f36
sahyadri551 <sahyaadri2413082@akgec.ac.in>
2025-06-26 13:49:37 +0530

    Remove unnecessary exception declaration from main method in Operators class

--- Learning/src/Operators.java
+++ Learning/src/Operators.java
@@ -1,5 +1,5 @@
 public class Operators {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
         System.out.println("Hello, World!");
     }
 }
